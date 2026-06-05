d842e1813e527a35f4ce635a82a75a8e7cb8eb26
sauva20 <2qistysauva@gmail.com>
2026-06-05 09:43:38 +0700

    hayuk

---
+++ app/Models/Pineapple.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pineapple extends Model
+{
+    protected $guarded = [];
+
+public function photos() {
+    return $this->hasMany(Photo::class);
+}
+
+public function user() {
+    return $this->belongsTo(User::class);
+}
+}
---
+++ database/migrations/2026_06_05_021211_create_photos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+public function up(): void
+{
+    Schema::create('photos', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('pineapple_id')->constrained()->cascadeOnDelete();
+        $table->enum('angle', ['atas', 'bawah', 'samping_kanan', 'samping_kiri', 'depan']);
+        $table->string('lokasi_lokal')->nullable();
+        $table->string('lokasi_drive')->nullable();
+        $table->string('status_upload')->default('pending'); // pending, uploading, done
+        $table->timestamps();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('photos');
+    }
+};
